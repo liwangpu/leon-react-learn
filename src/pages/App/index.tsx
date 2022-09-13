5dcf310e1e2ea36e2055390fb14c47b040ef3782
leon <puliwang@cxist.com>
2022-09-13 22:07:02 +0800

    添加动态路由

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { LazyExoticComponent, Suspense } from 'react';
 import './index.less';
 import { FileOutlined } from '@ant-design/icons';
 import { Routes, Route, NavLink, Navigate } from "react-router-dom";
@@ -8,52 +8,47 @@ const DynamicTest = React.lazy(() => import('../DynamicTest'));
 const SubRoute = React.lazy(() => import('../SubRoute'));
 const Hierarchy = React.lazy(() => import('../Hierarchy'));
 
+const routes: { title: string; path: string; component: LazyExoticComponent<any>; list?: boolean; }[] = [
+  {
+    title: 'Hook学习',
+    path: 'hooks',
+    list: true,
+    component: React.lazy(() => import('../HookLearn'))
+  },
+  {
+    title: '层级组件',
+    path: 'hierarchy',
+    list: true,
+    component: React.lazy(() => import('../Hierarchy'))
+  },
+];
+
 export default () => {
+
+  const routerLinks = () => routes.filter(r => r.list).map(r => (
+    <NavLink key={r.title} to={r.path} className="item">
+      <FileOutlined className='icon' />
+      <p>{r.title}</p>
+    </NavLink >
+  ));
+  const routerRegistries = () => routes.map(r => (
+    <Route key={r.title} path={r.path} element={
+      <Suspense fallback={<div>Loading...</div>}>
+        <r.component />
+      </Suspense>
+    } />
+  ));
 
   return (
     <div className='tutorial-app'>
       <div className="navs">
         <p className="title">React学习</p>
-        <NavLink to="dynamic-test" className="item">
-          <FileOutlined className='icon' />
-          <p>动态组件</p>
-        </NavLink >
-        <NavLink to="hierarchy" className="item">
-          <FileOutlined className='icon' />
-          <p>层级组件</p>
-        </NavLink >
-        <NavLink to="hooks" className="item">
-          <FileOutlined className='icon' />
-          <p>Hook</p>
-        </NavLink >
-        <NavLink to="sub-route" className="item">
-          <FileOutlined className='icon' />
-          <p>二级路由</p>
-        </NavLink >
+        {routerLinks()}
       </div>
       <div className="page">
         <Routes>
-          <Route path="/hooks" element={
-            <Suspense fallback={<div>Loading...</div>}>
-              <HookLearn />
-            </Suspense>
-          } />
-          <Route path="/dynamic-test" element={
-            <Suspense fallback={<div>Loading...</div>}>
-              <DynamicTest />
-            </Suspense>
-          } />
-          <Route path="/hierarchy" element={
-            <Suspense fallback={<div>Loading...</div>}>
-              <Hierarchy />
-            </Suspense>
-          } />
-          <Route path="/sub-route" element={
-            <Suspense fallback={<div>Loading...</div>}>
-              <SubRoute />
-            </Suspense>
-          } />
-          <Route path="*" element={<Navigate to="/dynamic-test" replace />} />
+          {routerRegistries()}
+          <Route path="*" element={<Navigate to="/hooks" replace />} />
         </Routes>
       </div>
     </div>
