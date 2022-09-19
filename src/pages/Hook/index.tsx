1f40cb478fe7b133f678a19e883922ef3ba76bb7
leon <liwangp2012@163.com>
2022-09-19 10:46:39 +0800

    1

--- src/pages/Hook/index.tsx
+++ src/pages/Hook/index.tsx
@@ -1,24 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { faker } from '@faker-js/faker';
 import styles from './index.module.less';
 
 export default function Hook(props: any): JSX.Element {
 
-  const [cardState, setCardState] = useState(true);
   const [title, setTitle] = useState(faker.name.fullName());
 
   return (
     <div className={styles['hook']}>
       <div className={styles['header']}>
         <button onClick={() => setTitle(faker.name.fullName())}>切换标题</button>
-        <button onClick={() => setCardState(!cardState)}>切换Card显隐状态</button>
       </div>
       <div className={styles['content']}>
         <p>{title}</p>
-        {cardState && (
-          <div>我是卡片</div>
-        )}
+        <NestedComponent title={title} />
       </div>
+    </div>
+  );
+}
+
+
+
+function NestedComponent(props: { title: string }) {
+
+  useEffect(() => {
+    console.log(`work:`,);
+  }, [props.title]);
+
+  return (
+    <div>
+      <p>Nested Component</p>
+      <p>{props.title}</p>
     </div>
   );
 }
