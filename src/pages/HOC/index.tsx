954e044bb47fc745944b1406bb20bacaa7c0dad6
leon <liwangp2012@163.com>
2022-09-19 18:56:30 +0800

    添加卡片

--- src/pages/HOC/index.tsx
+++ src/pages/HOC/index.tsx
@@ -9,18 +9,26 @@ import Counter from '../../components/Counter';
 
 export default function HOC(props: any): JSX.Element {
 
-  // const [cardState, setCardState] = useState(true);
+  const [cardVisible, setCardVisible] = useState(true);
   // const [title, setTitle] = useState(faker.name.fullName());
 
   return (
     <div className={styles['hoc']}>
       <div className={styles['header']}>
+        <button onClick={() => setCardVisible(!cardVisible)}>切换卡片显隐</button>
         {/* <button onClick={() => setTitle(faker.name.fullName())}>切换标题</button>
         <button onClick={() => setCardState(!cardState)}>切换Card显隐状态</button> */}
       </div>
       <div className={styles['content']}>
         <Counter />
+        {cardVisible && <Card />}
       </div>
     </div>
+  );
+}
+
+function Card(props: any): JSX.Element {
+  return (
+    <div>卡片</div>
   );
 }
