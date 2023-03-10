c40d2c136718bd739be50df6473988bf71c03e8e
蒲立望60263 <liwang.pu@going-link.com>
2023-03-10 18:10:56 +0800

    standard app取消严格模式

--- apps/standard/src/app/pages/HOC/index.tsx
+++ apps/standard/src/app/pages/HOC/index.tsx
@@ -3,23 +3,14 @@ import styles from './index.module.less';
 
 const HOC: React.FC = memo(() => {
 
-  const handleTest = () => {
-
-  };
 
   return (
     <div className={styles['page']}>
-      <div className={styles['page__header']}>
-        {/* <Button onClick={handleTest}>测试</Button> */}
-      </div>
-      <div className={styles['page__content']}>
 
-      </div>
     </div>
   );
 });
 
 HOC.displayName = 'HOC';
-
 
 export default HOC;
